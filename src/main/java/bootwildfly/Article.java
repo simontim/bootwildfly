22a86650bc387459784085883961c805d3c62b31
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-04-15 20:43:22 +0200

    Create Article.java

---
+++ src/main/java/bootwildfly/Article.java
@@ -0,0 +1,27 @@
+package bootwildfly;
+
+import org.springframework.data.annotation.Id;
+
+public class Article {
+
+	@Id private String id;
+
+	private String title;
+	private String content;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
