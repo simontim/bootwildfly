a7ab9713ecc90fb8d1e4ceeee112ecd3b0172c31
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-04-17 15:01:26 +0200

    Update ArticleRepository.java

--- src/main/java/bootwildfly/ArticleRepository.java
+++ src/main/java/bootwildfly/ArticleRepository.java
@@ -3,12 +3,10 @@ package bootwildfly;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "rilegatore", path = "rilegatore")
 public interface ArticleRepository extends MongoRepository<Article, String> {
 
-	List<Article> findByTitle(@Param("title") String title);
+	public Article findByTitle(String title);
+        public List<Article> findByContent(String content);
 
 }
